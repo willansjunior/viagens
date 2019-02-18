303f9eb83463693634c956fb49bb957e1790c653
Willans Junior <willans.junior@mv.com.br>
2019-02-18 13:47:22 -0300

    Tela de resumo da compra.

--- app/src/main/java/br/com/viagens/DetalhePagamentoActivity.java
+++ app/src/main/java/br/com/viagens/DetalhePagamentoActivity.java
@@ -3,6 +3,8 @@ package br.com.viagens;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -25,6 +27,8 @@ public class DetalhePagamentoActivity extends AppCompatActivity {
     private EditText edtCVCCartao;
     private EditText edtNomeCartao;
 
+    private Button btnPagamento;
+
     private Pacote pacote;
 
     @Override
@@ -45,6 +49,16 @@ public class DetalhePagamentoActivity extends AppCompatActivity {
         edtAnoCartao = findViewById(R.id.edt_pagamento_ano_cartao);
         edtCVCCartao = findViewById(R.id.edt_pagamento_cvc_cartao);
         edtNomeCartao = findViewById(R.id.edt_pagamento_nome_cartao);
+
+        btnPagamento = findViewById(R.id.btn_pagamento_finalizar);
+        btnPagamento.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DetalhePagamentoActivity.this, ResumoCompraActivity.class);
+                intent.putExtra(Constantes.PACOTE, pacote);
+                startActivity(intent);
+            }
+        });
     }
 
     private void montarDetalhe() {
---
+++ app/src/main/java/br/com/viagens/ResumoCompraActivity.java
@@ -0,0 +1,68 @@
+package br.com.viagens;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+import br.com.viagens.model.Pacote;
+import br.com.viagens.util.Constantes;
+
+public class ResumoCompraActivity extends AppCompatActivity {
+
+    private TextView cidade;
+    private TextView periodo;
+    private TextView preco;
+
+    private ImageView banner;
+
+    private Pacote pacote;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resumo_compra);
+
+        initViews();
+
+        montarDetalhe();
+    }
+
+    private void initViews() {
+        cidade = findViewById(R.id.txt_resumo_compra_cidade);
+        periodo = findViewById(R.id.txt_resumo_compra_periodo);
+        preco = findViewById(R.id.txt_resumo_compra_preco);
+
+        banner = findViewById(R.id.img_resumo_compra_banner_pacote);
+    }
+
+    private void montarDetalhe() {
+        Intent intent = getIntent();
+        pacote = (Pacote) intent.getSerializableExtra(Constantes.PACOTE);
+        if (pacote != null) {
+            cidade.setText(pacote.getCidade());
+
+            banner.setImageDrawable(getResources().getDrawable(getResources()
+                    .getIdentifier(pacote.getImagem(), Constantes.DRAWABLE, getPackageName())));
+
+            NumberFormat format = DecimalFormat.getCurrencyInstance(new Locale(
+                    getResources().getConfiguration().locale.getLanguage(),
+                    getResources().getConfiguration().locale.getCountry()));
+            preco.setText(format.format(pacote.getPreco()).replace("R$", "R$ "));
+        }
+
+        Calendar calendar = Calendar.getInstance();
+        Calendar volta = Calendar.getInstance();
+        volta.add(Calendar.DATE, pacote.getDias());
+        SimpleDateFormat format = new SimpleDateFormat("dd/MM");
+        periodo.setText(format.format(calendar.getTime()) + " " + format.format(volta.getTime()) +
+                " de " + volta.get(Calendar.YEAR));
+    }
+}
